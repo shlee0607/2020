0e1577524852c73cf2e1b620ca0f4ec9af5fde13
Jungrae Kim <dale40@skku.edu>
2020-03-19 20:11:51 +0900

    Updated alu.s

--- lab3/alu.s
+++ lab3/alu.s
@@ -1,14 +1,32 @@
+@ Copyright (c) 2020 Jungrae Kim <dale40@skku.edu>
+@ This file is for programming assignments of micro-processor class at SungKyunKwan University.
+
+@ This template mimics a C program of
+@ #include <stdio.h>
+@ int main()
+@ {
+@     printf("Output is %d\n", 1);
+@     return 0;
+@ }
+
 	.global main
 main:
-	push	{lr}
+	push	{lr}		@ temporarily store the link register value into the stack
 
-	ldr	r0, =message
-	mov	r1, #1
-	bl	printf
+				@ Your ALU codes start here
 
-	mov	r0, #0
-	pop	{pc}
+				@ Your codes must NOT update the special registers (SP, LR, PC).
+				@ Your codes must put the output into r0, to be printed by the printf()
 
-message:
-	.align	2
+				@ Your ALU codes end here
+
+	ldr	r0, =message	@ the message string is the first argument of the printf()
+	mov	r1, #1		@ value of 1 is the second argument of the printf()
+	bl	printf		@ invoke the printf()
+
+	mov	r0, #0		@ this is the return value (0) of this main function
+	pop	{pc}		@ pop the saved link register value to update the PC to return
+
+message:			@ data part
+	.align	2		@ data are aligned to 2 bytes (16 bits)
 	.asciz 	"Output is %d\012\000"
